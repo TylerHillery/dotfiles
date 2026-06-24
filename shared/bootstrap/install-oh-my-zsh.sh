33371051c0454a6959da54c0355d2114fcf7adff
Tyler Hillery <tyhillery@gmail.com>
2026-06-24 10:00:29 -0500

    feat: stuff

--- shared/bootstrap/install-oh-my-zsh.sh
+++ shared/bootstrap/install-oh-my-zsh.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-if [ -d "$HOME/.oh-my-zsh" ]; then
+if [ -f "$HOME/.oh-my-zsh/oh-my-zsh.sh" ]; then
 	echo "oh-my-zsh already installed"
 	exit 0
+fi
+
+if [ -d "$HOME/.oh-my-zsh" ]; then
+	echo "Removing incomplete oh-my-zsh install..."
+	rm -rf "$HOME/.oh-my-zsh"
 fi
 
 echo "Installing oh-my-zsh..."
